bdc50a9e7d8fa2da1c2bcc2335421649ef423ace
Justin Locke <Justindeanlocke@gmail.com>
2024-06-25 18:10:55 -0500

    Lightly changed classes to remove redundencies.

--- Calculator.java
+++ Calculator.java
@@ -1,26 +1,22 @@
 public class Calculator {
 
     public static double add(double a, double b) {
-        double addResult = a + b;
         //System.out.println(a + " plus " + b + " equals " + addResult);
-        return addResult;
+        return a + b;
     }
     
     public static double subtract(double a, double b) {
-        double subtractResult = a - b;
         //System.out.println(a + " minus " + b + " equals " + subtractResult);
-        return subtractResult;
+        return a - b;
     }
     public static double multiply(double a, double b) {
-        double multiplyResult = a * b;
         //System.out.println(a + " multiplied by " + b + " equals " + multiplyResult);
-        return multiplyResult;
+        return a * b;
     }
     public static double divide(double a, double b) {
-        double divideResult = a / b;
-        
+
         //System.out.println(a + " divided by " + b + " equals " + divideResult);
-        return divideResult;
+        return a / b;
     }
 
     public static double answer(double a) {
